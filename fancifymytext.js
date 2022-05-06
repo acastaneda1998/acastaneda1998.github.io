afc4568874fcd7fa25e9b3ffbcebc35113d0ff4c
acastaneda1998 <61564492+acastaneda1998@users.noreply.github.com>
2022-05-05 18:50:31 -0700

    Add files via upload

---
+++ fancifymytext.js
@@ -0,0 +1,38 @@
+function Bigger(){
+    document.getElementById("inputText").style.fontSize = "24pt";
+}
+
+function Smaller(){
+    document.getElementById("inputText").style.fontSize = "6pt";
+}
+
+function Medium(){
+    document.getElementById("inputText").style.fontSize = "10pt";
+}
+
+function FancyShmancy(){
+    document.getElementById("inputText").style.fontWeight = "bold";
+    document.getElementById("inputText").style.color = "blue";
+    document.getElementById("inputText").style.textDecoration = "underline";
+}
+
+function Boring(){
+    document.getElementById("inputText").style.fontWeight = "normal";
+    document.getElementById("inputText").style.color = "black";
+    document.getElementById("inputText").style.textDecoration = "none";
+}
+
+function Moo(){
+    const text = document.getElementById("inputText");
+    text.value = text.value.toUpperCase();
+
+    var str = text.value;
+    var parts = str.split("."); 
+    str = parts.join("-Moo"); 
+
+    text.value = str;
+}
+
+function alertTest(){
+    alert("Hello, World!");
+}
